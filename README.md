7fa1a5d63b6ec5de3ba9a055b437dc4de319c5f2
Alexander Corn <mckay@doctormckay.com>
2015-12-22 23:06:54 -0500

    1.0.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Steam Community Profile SteamID Viewer
+
+Adds an option to the "More" dropdown (or a button on your own profile) to view a user's SteamID in all 3 formats, plus the permalink. Also allows one-click copying.
+
+Requires Greasemonkey on Firefox and Tampermonkey on Chrome.
+
+## [![Install](https://i.imgur.com/hKHfyWz.png)](https://raw.githubusercontent.com/DoctorMcKay/steam-profile-steamid-viewer/master/profile_steamid_viewer.user.js)
---
+++ profile_steamid_viewer.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name        Steam Community Profile SteamID Viewer
+// @namespace   www.doctormckay.com
+// @description Shows a user's SteamIDs on their profile
+// @include     *://steamcommunity.com/id/*
+// @include     *://steamcommunity.com/id/*/
+// @include     *://steamcommunity.com/profiles/*
+// @include     *://steamcommunity.com/profiles/*/
+// @version     1.0.0
+// @require     https://www.traderep.org/js/modules.min.js
+// @grant       GM_setClipboard
+// ==/UserScript==
+
+document.addEventListener('DOMContentLoaded', function() {
+	if(typeof unsafeWindow.g_rgProfileData === 'undefined' || !unsafeWindow.g_rgProfileData.steamid) {
+		return; // guess this isn't a profile
+	}
+	
+	var dropdown = document.querySelector('#profile_action_dropdown .popup_body.popup_menu');
+	if(dropdown) {
+		// We have a dropdown
+		dropdown.innerHTML += '<a class="popup_menu_item" href="javascript:OpenSteamIdDialog()"><img src="https://i.imgur.com/9MQ0ACl.png"> View SteamID</a>';
+	} else {
+		var actions = document.querySelector('.profile_header_actions');
+		if(actions) {
+			actions.innerHTML += '<a class="btn_profile_action btn_medium" href="javascript:OpenSteamIdDialog()" title="View SteamID">' +
+				'<span><img src="https://i.imgur.com/9MQ0ACl.png" style="width: 16px; height: 16px; margin: 7px 0; vertical-align: top"></span></a> ';
+		}
+	}
+	
+	var idDialog;
+	unsafeWindow.OpenSteamIdDialog = exportFunction(function() {
+		unsafeWindow.HideMenu('profile_action_dropdown_link', 'profile_action_dropdown');
+		
+		var sid = new Modules.SteamID(unsafeWindow.g_rgProfileData.steamid);
+		var html = '<div class="bb_h1">Click to copy</div>';
+		html += '<p><a href="javascript:CopyToClipboard(\'' + sid.getSteam2RenderedID() + '\')">' + sid.getSteam2RenderedID() + '</a></p>';
+		html += '<p><a href="javascript:CopyToClipboard(\'' + sid.getSteam3RenderedID() + '\')">' + sid.getSteam3RenderedID() + '</a></p>';
+		html += '<p><a href="javascript:CopyToClipboard(\'' + sid.getSteamID64() + '\')">' + sid.getSteamID64() + '</a></p>';
+		html += '<p><a href="javascript:CopyToClipboard(\'https://steamcommunity.com/profiles/' + sid.getSteamID64() + '\')">https://steamcommunity.com/profiles/' + sid.getSteamID64() + '</a></p>';
+		
+		idDialog = unsafeWindow.ShowAlertDialog(unsafeWindow.g_rgProfileData.personaname + "'s SteamID", html, "Close");
+	}, unsafeWindow);
+	
+	unsafeWindow.CopyToClipboard = exportFunction(function(text) {
+		GM_setClipboard(text);
+		unsafeWindow.ShowAlertDialog("Text Copied", '"' + text + '" has been copied to your clipboard.', "OK");
+		idDialog.Dismiss();
+	}, unsafeWindow);
+});
